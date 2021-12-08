20cc8688381348e6a3b88af4070962d9102fbfd5
Jean-Christophe MICHEL <jc.michel@symetrie.com>
2021-12-08 23:20:05 +0100

    correct setup

--- sale.py
+++ sale.py
@@ -9,7 +9,7 @@ from trytond.transaction import Transaction
 __all__ = ['Line']
 
 
-class Line:
+class Line(metaclass=PoolMeta):
     __name__ = 'sale.line'
 
     list_price = fields.Numeric('List Price', digits=(16, 4),
